afa11db216af94b18653de12cfb731c290e8d9ab
kiyani <bamba.kiyani@gmail.com>
2020-03-11 14:25:00 -0400

    updated links

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -30,7 +30,7 @@ const Body = () => {
                     It was tested with a group of high school girls. After a series of interviews with these young girls, I realized that my solution was not the one they needed and I closed the bot. I wrote about my though process in an article (it's in french <i class="em em-laughing" aria-role="presentation" aria-label="SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES"></i> ).
                     </p>
                     <div className="links-demo">
-                        <a href="https://kiyanibamba.com/2018/10/"> Article. </a>
+                        <a href="https://kiyanib.wordpress.com/2018/10/03/experimentation-001-chatbot-pour-reduire-les-grossesses-precoses-en-ci/"> Article. </a>
                         <a href="https://app.botsociety.io/s/5ba3aab753567c07abee2ecd?p=f2ce95c3850bdce978637e78392916832a583eb9"> Demo. </a>
                     </div>
 
