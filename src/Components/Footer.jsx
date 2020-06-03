01f15609c4162c7fbe1d45331a7e3b3ef2b4b04c
kiyani <bamba.kiyani@gmail.com>
2020-06-03 08:49:55 -0400

    stuff footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
             </h3>
             <FooterDivLink>
 				<a  href="mailto:bamba.kiyani@gmail.com">Email</a>
-				<a  href="https://twitter.com/KiyaniBba">Twitter</a>
                 <a  href="https://github.com/KiyaniBamba?tab=repositories">GitHub</a>
-                <a  href="https://kiyanirandomfinds.substack.com/about?utm_source=menu-dropdown">Substack</a>
 				<a  href="https://medium.com/@bamba.kiyani">Medium</a>
+                <a  href="https://kiyanirandomfinds.substack.com/about?utm_source=menu-dropdown">Substack</a>
+				<a  href="https://twitter.com/KiyaniBba">Twitter</a>
 
             </FooterDivLink>
 
