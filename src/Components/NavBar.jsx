acd2a35f8eae981b88faec7d509ce40168b33a3a
kiyani <bamba.kiyani@gmail.com>
2020-07-31 14:58:50 -0400

    fini

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
                 </DivNav1>
                 <DivNav2>
                     <a class="nav-item" href="/about"> about </a>
-                    <a class="nav-item" href="https://drive.google.com/file/d/1FGJjxwrRBc14FY1qqjdhqy6a12OgwzC6/view?usp=sharing">resume</a>
+                    <a class="nav-item" href="https://drive.google.com/file/d/1nbP0bLW_DP1_TPlFznk26N0TAMVsr675/view?usp=sharing">resume</a>
                     {/* <a class="nav-item" href="https://medium.com/@bamba.kiyani">medium</a> */}
                     <a class="nav-item" href="mailto:bamba.kiyani@gmail.com"> email</a>
                 </DivNav2>
